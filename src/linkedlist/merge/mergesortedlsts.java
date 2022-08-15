29c831b86bbeff269e096f45a57d76e2a7674903
sleepycan <273099906@qq.com>
2022-08-15 21:36:33 +0800

    LEETCODE21 合并有序链表 本次提交完成：头插法+链表打印+完成题目

--- src/linkedlist/merge/mergesortedlsts.java
+++ src/linkedlist/merge/mergesortedlsts.java
@@ -1,18 +1,52 @@
 package linkedlist.merge;
 
+import java.util.List;
+
 public class mergesortedlsts {
     public static void main(String[] args){
-        int[] num1 = {1,3,4,5};
-        int[] num2 = {2,5,7,10};
-
-
+        int[] num1 = {10,6,2,1};
+        int[] num2 = {12,9,7,3,0};
+        ListNode list1 = create(num1);
+        ListNode list2 = create(num2);
+        ListNode head = mergeTwoLists(list1,list2);
+        while(head!=null){
+            System.out.println(head.val);
+            head = head.next;
+        }
     }
-    public ListNode mergeTwoLists(ListNode List1, ListNode List2){
+    public static ListNode create(int[] num){
+        ListNode head = null;
+        for(int i=0;i<num.length;i++){
+            ListNode list = new ListNode(num[i]);
+            if(head==null)head=list;
+            else{
+                list.next = head;
+                head = list;
+            }
+        }
+        return head;
+    }
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2){
         ListNode dummy = new ListNode(-1);
         ListNode p = dummy;
-        ListNode p1 = List1;
-
-
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        while(p1!=null&&p2!=null){
+            if(p1.val>p2.val){
+                p.next = p2;
+                p2 = p2.next;
+            }else{
+                p.next = p1;
+                p1 = p1.next;
+            }
+            p = p.next;
+        }
+        if(p1==null){
+            p.next = p2;
+        }
+        if(p2==null){
+            p.next = p1;
+        }
         return dummy.next;
     }
 }
