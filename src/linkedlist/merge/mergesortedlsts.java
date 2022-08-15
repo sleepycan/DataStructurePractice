212a102623acf2db15bb11d6e50b11ce5cc8e266
sleepycan <273099906@qq.com>
2022-08-15 21:06:58 +0800

    write ListNode

--- src/linkedlist/merge/mergesortedlsts.java
+++ src/linkedlist/merge/mergesortedlsts.java
@@ -1,4 +1,20 @@
 package linkedlist.merge;
 
 public class mergesortedlsts {
+    public static void main(String[] args){
+        int[] num1 = {1,3,4,5};
+        int[] num2 = {2,5,7,10};
+
+
+    }
+    public ListNode mergeTwoLists(ListNode List1, ListNode List2){
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        ListNode p1 = List1;
+
+
+        return dummy.next;
+    }
 }
+
+
